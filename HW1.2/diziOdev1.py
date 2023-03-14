4de3ac35bd9c63bd03457ff97d41c858c0401253
Emirhan Yücel <47223372+emirhanyucel@users.noreply.github.com>
2023-03-14 17:54:33 +0300

    "Dizi Temelli Yapıları Gerçek Hayatla İlişkilendirelim" ve "Döngüleri Gerçek Hayatla İlişkilendirip Öğrenelim"

---
+++ HW1.2/diziOdev1.py
@@ -0,0 +1,14 @@
+ogrenciler = ["Ali","Emre","Ayşe","Deniz"]
+
+print(ogrenciler)
+
+print(ogrenciler[0])
+print(ogrenciler[1])
+print(ogrenciler[2])
+print(ogrenciler[3])
+
+print(len(ogrenciler))
+
+ogrenciler[0] = "Batuhan"
+
+print(ogrenciler)
---
+++ HW1.2/diziOdev2.py
@@ -0,0 +1,19 @@
+#Hepsiburada sitesinden dizilere verilebilecek örnekler.
+
+aradiginMarkalar = ["Arzum","June","Regal","Joystar","Karaca"]
+
+erkekGiyim = ["Pantolon","Gömlek","Tshirt","Kazak","Mont"]
+
+print(aradiginMarkalar)
+print(erkekGiyim)
+
+print(len(erkekGiyim))
+
+aradiginMarkalar[0] = "Apple"
+
+print(aradiginMarkalar)
+print(erkekGiyim[0])
+print(erkekGiyim[1])
+print(erkekGiyim[2])
+print(erkekGiyim[3])
+print(erkekGiyim[4])
