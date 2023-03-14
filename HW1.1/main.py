1a90adcd3b3661c1359cc9c8ccc4ac5df627e7f3
Emirhan Yücel <47223372+emirhanyucel@users.noreply.github.com>
2023-03-14 15:42:17 +0300

    Create main.py

---
+++ HW1.1/main.py
@@ -0,0 +1,44 @@
+#Python Veri Türleri / Python data types
+
+#str "String", metin türü veri
+veri1 = "MetinselVeri"
+
+#int "Integer", tamsayı
+veri2 = 10
+
+#float "Float", ondalıklı sayı
+veri3 = 3.14
+
+#bool "Boolean", True/False mantıksal veri
+veri4 = True
+veri5 = 3 > 2
+# #print(veri5)
+# Bu komut üç ikiden büyük olduğu
+# için True olarak terminal'e yansıyacaktır.
+############################################
+
+# Kodlama.io sitesi değişken örnekleri ve veri tipleri
+# 1. Kurs ve eğitmen isimleri metinsel str "String" değişkenler.
+
+# 2. Bitir ve devam et butonuna tıklandıysa buttonClicked = True şeklinde boolean verili değişken tipi.
+# Aynı şekilde bir kursa başlama butonuna bastıysak kullanıcıBuKurstaMi = True
+# şeklinde Tüm kurslar sekmesinden kurslarım sekmesine geçişi sağlanır. Bu da 
+# boolean true/false veri tipi değişkene örnek olabilir.
+
+# 3. Değerlendirme kısmında yorum sayısı integer/tam sayı veri tipi. 130.5 yorum diye bir durum
+# söz konusu olamayacağı için float olamaz.
+
+# 4. Kursun tamamlanma seviyesi %25 iken 0.25 olduğu için float,
+# yani ondalıklı sayı veri tipli değişkendir.
+#############################################
+#Kodlama.io sitesi şart bloklarına örnek
+
+# Ödevleri yaparken "Bitir ve devam et" butonuna bastığımızda soldaki listede
+# logonun tik işaretine dönüşmesi.
+
+# if buttonClicked = True:
+#     logo = "tikIsareti.png"
+# else:
+#     logo = "boşDaire.png"
+
+
